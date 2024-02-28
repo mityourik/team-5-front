2058eb98427dd7018f87571f85024fe2f7eeb272
Dmitry <mityourik@gmail.com>
2024-02-29 02:07:42 +0300

    added filterbar to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import { useState } from 'react';
 // import Header from './components/Header/Header';
 // import NavBar from './components/NavBar/NavBar';
-import DropdownButton from './UI/Buttons/DropdownButton1/DropdownButton1';
-import FilterMailingBtn from './UI/Buttons/FilterMailingBtn';
-import SearchBar from './UI/SearchBar/SearchBar';
-import imgFilter from './UI/Buttons/assets/AmbFilterButtonImg.svg';
+// import DropdownButton from './UI/Buttons/DropdownButton1/DropdownButton1';
+// import FilterMailingBtn from './UI/Buttons/FilterMailingBtn';
+// import SearchBar from './UI/SearchBar/SearchBar';
+// import imgFilter from './UI/Buttons/assets/AmbFilterButtonImg.svg';
+
+import Filter from './components/FilterBar/Filter';
 
 function App() {
   // const [isOpen, setIsOpen] = useState(false);
@@ -12,40 +14,25 @@ function App() {
   // const handleMouseEnter = () => setIsOpen(true);
   // const handleMouseLeave = () => setIsOpen(false);
 
-  const handleSearch = (searchTerm) => {
-    console.log('Поиск:', searchTerm);
-  };
+  // const handleSearch = (searchTerm) => {
+  //   console.log('Поиск:', searchTerm);
+  // };
 
-  const menuOptions = [
-    {
-      label: 'Добавить вручную',
-      action: () => console.log('Добавление вручную'),
-    },
-    {
-      label: 'Импортировать',
-      action: () => console.log('Импорт'),
-    },
-  ];
+  // const menuOptions = [
+  //   {
+  //     label: 'Добавить вручную',
+  //     action: () => console.log('Добавление вручную'),
+  //   },
+  //   {
+  //     label: 'Импортировать',
+  //     action: () => console.log('Импорт'),
+  //   },
+  // ];
 
   return (
-    <>
-      {/* <Header isOpen={isOpen} />
-      <NavBar
-        isOpen={isOpen}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      /> */}
-      <SearchBar
-        onSearch={handleSearch}
-      />
-      <DropdownButton
-        buttonLabel="Добавить амбассадора"
-        menuOptions={menuOptions}
-      />
-      <FilterMailingBtn
-        backgroundImage={imgFilter}
-      />
-    </>
+    <div style={{ padding: '64px' }}>
+      <Filter />
+    </div>
   );
 }
 
