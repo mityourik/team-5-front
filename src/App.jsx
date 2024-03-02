cb17ba0558fa8b706426aaf6800c279818b5f801
Dmitry <mityourik@gmail.com>
2024-03-02 21:24:12 +0300

    hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-import MainPagePanel from './components/MainPagePanel/MainPagePanel';
 import AmbassadorPage from './components/AmbassadorPage/AmbassadorPage';
+import HeaderSidebarLayout from './components/LayoutHeaderSidebar/HeaderSidebarLayout';
+import TabsNavigation from './components/TabsNavigation/TabsNavigation';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/team-5-front/" element={<MainPagePanel />} />
-      <Route path="/team-5-front/ambassador-page" element={<AmbassadorPage />} />
-    </Routes>
+    <>
+      <HeaderSidebarLayout>
+        <TabsNavigation />
+      </HeaderSidebarLayout>
+      <Routes>
+        <Route path="/team-5-front/ambassador-page" element={<AmbassadorPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/TabsNavigation/TabsNavigation.jsx
@@ -0,0 +1,49 @@
+// import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import './TabsNavigation.scss';
+import { useState } from 'react';
+
+function TabsNavigation() {
+  const [count] = useState('');
+
+  return (
+    <main className="main">
+      <section className="navigation-tabs">
+        <nav aria-label="Основная навигация сайта" className="navigation-tabs__container">
+          <ul className="navigation-tabs__list">
+            <li className="navigation-tabs__item">
+              <NavLink to="/ambassadors" className="navigation-tabs__link">
+                Амбассадоры
+                <span className="navigation-tabs__link-count">{count}</span>
+              </NavLink>
+            </li>
+            <li className="navigation-tabs__item">
+              <NavLink to="/content" className="navigation-tabs__link">
+                Контент
+                <span className="navigation-tabs__link-count">{count}</span>
+              </NavLink>
+            </li>
+            <li className="navigation-tabs__item">
+              <NavLink to="/merch-sending" className="navigation-tabs__link">
+                Отправка мерча
+                <span className="navigation-tabs__link-count">{count}</span>
+              </NavLink>
+            </li>
+            <li className="navigation-tabs__item">
+              <NavLink to="/merch-budget" className="navigation-tabs__link">
+                Бюджет на мерч
+                <span className="navigation-tabs__link-count">{count}</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </main>
+  );
+}
+
+// TabsNavigation.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
+
+export default TabsNavigation;
