bc929e6d1bcaeaf6007845cdd1a4f13f0e7b1b27
Dmitry <mityourik@gmail.com>
2024-03-09 14:32:28 +0300

    content page is ok with no popup

--- src/services/selectors/ambassadorSelector.js
+++
@@ -1,4 +0,0 @@
-export const getAmbassadorData = (state) => state.ambassador.ambassadorData;
-export const getIsAmbassadorDataEditing = (state) => state.ambassador.isAmbassadorDataEditing;
-export const getIsLoadingAmbassadorData = (state) => state.ambassador.isLoadingAmbassadorData;
-export const getIsNewAmbassadorAdding = (state) => state.ambassador.isNewAmbassadorAdding;
