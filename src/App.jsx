74b394c0fd8da3d5d163e93e45ec16722d98294c
Dmitry <mityourik@gmail.com>
2024-03-09 16:06:55 +0300

    resolve руты

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <Routes>
       <Route
-        path="/"
+        path="/ambassadors"
         element={(
           <HeaderSidebarLayout>
             <AmbassadorsPage />
