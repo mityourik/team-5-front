b4c1394986bd95b061615622b3ea21caf6952f6a
Dmitry <mityourik@gmail.com>
2024-03-10 19:22:00 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ambassadors/',
+  base: '/team-5-front/',
   css: {
     preprocessorOptions: {
       scss: {
