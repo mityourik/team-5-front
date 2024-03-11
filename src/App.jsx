d8b221945ecda49f6633a96995b57e9e7d2fce4b
Dmitry <mityourik@gmail.com>
2024-03-11 08:59:28 +0300

    route / fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-// import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-// import { useDispatch } from 'react-redux';
-// import { fetchContent } from './services/thunks/ambassadorThunk';
 import AmbassadorsPage from './components/AmbassadorsPage/AmbassadorsPage';
 import AmbassadorPage from './components/AmbassadorPage/AmbassadorPage';
 import NewAmbassadorPage from './components/NewAmbassadorPage/NewAmbassadorPage';
 import HeaderSidebarLayout from './components/LayoutHeaderSidebar/HeaderSidebarLayout';
 import ContentPage from './components/ContentPage/ContentPage';
-// import ContentPage from './components/ContentPage/ContentPage';
 
 function App() {
   return (
     <Routes>
+      <Route
+        path="/"
+        element={(
+          <HeaderSidebarLayout>
+            <AmbassadorsPage />
+          </HeaderSidebarLayout>
+      )}
+      />
       <Route
         path="/ambassadors"
         element={(
